6dd2fcaa2d89bf8e93e39d8b794c3a392e0e0961
neilsen-ngoi <kenisu222@gmai.com>
2024-04-23 11:08:00 +1200

    added sheets/sidebar

--- components/mobile-header.tsx
+++ components/mobile-header.tsx
@@ -1,9 +1,9 @@
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const MobileHeader = () => {
   return (
-<nav className=" lg:hidden px6 h-[50px] flex items-center bg-yellow-500 border-b fixed top-0 w-full z-50">
-mobile header
-</nav>
-
- )
-}
+    <nav className=" lg:hidden px-4 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50">
+      <MobileSidebar />
+    </nav>
+  );
+};

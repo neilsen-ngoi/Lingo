69e30f76f4dac91e7b0f03b2054b4e85d0324bcc
neilsen-ngoi <kenisu222@gmai.com>
2024-04-29 15:18:41 +1200

    added course buttons to sticky feed

--- app/(main)/learn/header.tsx
+++ app/(main)/learn/header.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { ArrowLeft, Ghost } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
 type Props = {
@@ -8,7 +8,7 @@ type Props = {
 
 export const Header = ({ title }: Props) => {
   return (
-    <div className=" sticky top-0 bg-white pb-3 lg:pt-[28px] flex items-center justify-between border-b-2 text-neutral-400 lg:z-50">
+    <div className=" sticky top-0 bg-white pb-3 lg:pt-[28px] lg:mt-[-28px] flex items-center justify-between border-b-2 text-neutral-400 lg:z-50">
       <Link href={"/courses"}>
         <Button variant={"ghost"} size={"sm"}>
           <ArrowLeft className=" h-5 w-5 stroke-2 text-neutral-400" />
